151bfc31c69a7b48c9533405f8c09142e9ff35d9
KyValm <90875583+KyValm@users.noreply.github.com>
2022-09-15 13:40:06 -0700

    changes to the client.js class

--- Frontend/src/api/exampleClient.js
+++ Frontend/src/api/exampleClient.js
@@ -10,7 +10,7 @@ import axios from 'axios'
  * https://javascript.info/mixins
  */
 
-// Ky is here to work on this :D
+// Ky is here to work on this :D part 1 commit
 export default class ExampleClient extends BaseClass {
 
     constructor(props = {}){
