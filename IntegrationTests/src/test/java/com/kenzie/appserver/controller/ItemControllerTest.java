d6ddbd6b12de74357b95011255b0d98585a6601b
KyValm <90875583+KyValm@users.noreply.github.com>
2022-10-05 00:17:35 -0700

    finished some unit tests

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/ItemControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/ItemControllerTest.java
@@ -107,7 +107,7 @@ class ItemControllerTest {
                 .andExpect(jsonPath("itemId")
                         .exists())
                 .andExpect(jsonPath("description")
-                        .value(is(item.getDescription())))
+                        .value(is("Item is updated for testing purposes")))
                 .andExpect(jsonPath("currentQty")
                         .value(is(newItem.getCurrentQty())))
                 .andExpect(jsonPath("reorderQty")
